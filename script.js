cfc31ad6b840ca046b07d87a29edf4bd7c36c33a
Abdimalik11 <ilkacaze5522@gmail.com>
2023-06-27 11:45:42 -0700

    this is the answer of second-largest

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 // Good Luck 💪🏾
+function secondLargest(numbers) {
+    let largest = -Infinity;
+    let secondLargest = -Infinity;
+  
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] > largest) {
+        secondLargest = largest;
+        largest = numbers[i];
+      } else if (numbers[i] > secondLargest && numbers[i] !== largest) {
+        secondLargest = numbers[i];
+      }
+    }
+  
+    return secondLargest;
+  }
+console.log(secondLargest([23, 66, 50, 12, 6]));   
+console.log(secondLargest([65, 34, 100, 22, 85])); 
+console.log(secondLargest([67, 22, 19, 12, 80]));  
